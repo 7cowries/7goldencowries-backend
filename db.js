0fe82e9d5bf83368045cff514a3ed7f9d1be6148
7cowries <7goldencowries@gmail.com>
2025-07-31 11:04:49 +0100

    🔧 Finalize backend fixes with async db + updated routes

--- db.js
+++ db.js
@@ -1,9 +1,15 @@
 import sqlite3 from 'sqlite3';
 import { open } from 'sqlite';
 
-const db = await open({
-  filename: './cowries.db',
-  driver: sqlite3.Database
-});
+let db;
+
+const initDB = async () => {
+  db = await open({
+    filename: './cowries.db',
+    driver: sqlite3.Database
+  });
+};
+
+await initDB();
 
 export default db;
