aa3cdf53de2bb2ed23284e8dec0317d22eafa6f8
7cowries <7goldencowries@gmail.com>
2025-11-08 15:08:31 +0100

    feat(api): add /quests -> 307 /api/quests alias for compatibility

--- index.js
+++ index.js
@@ -128,6 +128,8 @@ const questsAlias = require('./routes/alias-quests.cjs');
 app.use('/quests', questsAlias);
 const questsAlias = require('./routes/alias-quests.cjs');
 app.use('/quests', questsAlias);
+const questsAlias = require('./routes/alias-quests.cjs');
+app.use('/quests', questsAlias);
 app.use(referralRoutes);
 app.use("/api/subscribe", subscriptionRoutes);
 app.use("/api", twitterRoutes);
